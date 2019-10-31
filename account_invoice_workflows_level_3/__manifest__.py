9633ef0b87be43a00435391a97f004595779502b
HafeezBrothers <mailto:hafeezbrothers.odoo@gmail.com>
2019-10-31 10:17:17 +0500

    My first File

---
+++ account_invoice_workflows_level_3/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name':   'Accounting Workflows (Level 3)',
+    'author': 'Hafeez Brothers',
+    'version': '1.0',
+    'depends': [
+                'base',
+                'account',
+               
+                
+                ],
+    
+    'data': [
+      
+        'Views/account_invoice_workflows.xml',
+        'security/hr_user_rights_invoice.xml'
+    ],
+    
+    'application': True,
+    'price':50.00,
+    'currency':'EUR', 
+
+}
